17763123a41a886583704bdb96988f9708e3b081
Atul Pandey <101567257+AtulPandey429@users.noreply.github.com>
2023-09-20 21:15:58 +0530

    connect

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-// const path = require("path");
+const path = require("path");
 
 //dotenv configuartion
 dotenv.config();
@@ -14,14 +14,14 @@ app.use(cors());
 app.use(express.json());
 
 // static files access
-// app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 //routes
 app.use("/api/v1/portfolio", require("./routes/portfolioRoute"));
 
-// app.get("*", function (req, res) {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 //port
 const PORT = process.env.PORT || 8080;
